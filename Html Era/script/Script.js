d3fb6ec0f974af6e1a4437334d0d1dc0d5d4a0ad
FACTORY132 <65250110+FACTORY132@users.noreply.github.com>
2024-01-19 16:41:51 +0700

    Add files via upload

--- Html Era/script/Script.js
+++ Html Era/script/Script.js
@@ -1,26 +1,19 @@
-window.onscroll = () => slide()
+window.onscroll = () => slide();
 
+const slideAt = (value) =>
+  document.documentElement.scrollTop > value || document.body.scrollTop > value;
 
-const slideAt = (value) => document.documentElement.scrollTop > value || document.body.scrollTop > value;
 
 function slide() {
+  let g = document.querySelector("nav");
+  let m = document.querySelector(".text-content");
+  let k = document.querySelector(".img-content");
+  if (slideAt(20)) g.style.top = "0";
 
-let g = document.querySelector('nav');
-let m = document.querySelector('.text-content')
-let k = document.querySelector('.img-content')
-let o = document.querySelector('footer')
-if(slideAt(20))
-   g.style.top = '0'
-
-   if(slideAt(400)){
-      m.style.left = '16rem'
-      m.style.opacity = '1';
-      k.style.left = '2.5%'
-      k.style.rotate = '0deg'
-   }
-
-   if(slideAt(700)) {
-      o.style.backgroundColor = 'black'
-   }
-
+  if (slideAt(400)) {
+    m.style.left = "16rem";
+    m.style.opacity = "1";
+    k.style.left = "2.5%";
+    k.style.rotate = "0deg";
+  }
 }
